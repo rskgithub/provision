f9e558810ef8ad6bd7a4cbeb3298431336fe6d73
Jay Doane <jay.s.doane@gmail.com>
2012-04-24 21:40:56 -0700

    Add example of devstack sizes

--- test/test_size_from_name.py
+++ test/test_size_from_name.py
@@ -3,8 +3,14 @@ import unittest
 import provision.nodelib as nodelib
 
 '''
+Below are some examples of sizes from real providers:
+
+>>> [(s.name, s.id) for s in devstack_driver().list_sizes()]
+[('m1.medium', '3'), ('m1.micro', '0'), ('m1.small', '2'), ('m1.large', '4'), ('m1.tiny', '1'), ('m1.xlarge', '5')]
+
 >>> [(s.name, s.id) for s in rackspace_driver().list_sizes()]
 [('256 server', '1'), ('512 server', '2'), ('1GB server', '3'), ('2GB server', '4'), ('4GB server', '5'), ('8GB server', '6'), ('15.5GB server', '7'), ('30GB server', '8')]
+
 >>> [(s.name, s.id) for s in aws_driver().list_sizes()]
 [('Medium Instance', 'm1.medium'), ('Large Instance', 'm1.large'), ('High-CPU Extra Large Instance', 'c1.xlarge'), ('Small Instance', 'm1.small'), ('High-CPU Medium Instance', 'c1.medium'), ('Extra Large Instance', 'm1.xlarge'), ('High-Memory Extra Large Instance', 'm2.xlarge'), ('Micro Instance', 't1.micro'), ('High-Memory Quadruple Extra Large Instance', 'm2.4xlarge'), ('High-Memory Double Extra Large Instance', 'm2.2xlarge')]
 '''
