b670d95b3d5272e358d2cc4d24ffeabfbe27656d
Jay Doane <jay.s.doane@gmail.com>
2012-07-08 21:56:31 -0700

    Add curl dependency

--- provision/defaults/scripts/datablox.sh
+++ provision/defaults/scripts/datablox.sh
@@ -1,8 +1,9 @@
+set -ex
 apt-get update
 # engage dependencies
 apt-get -y install git-core g++ ocaml zlib1g-dev python-dev python-crypto \
-    python-virtualenv make libzmq-dev libzmq1
+    python-virtualenv make libzmq-dev libzmq1 curl
 pip install pyzmq
 git clone git://github.com/mpi-sws-rse/datablox.git ~/datablox
 cd ~/datablox && make all
-cd ~/datablox/engage/engage-dist && ./install_datablox.py ~/apps
+#cd ~/datablox/engage/engage-dist && ./install_datablox.py ~/apps
